c654cf2ed77abe20671cb5ad69c28eafa6980762
breakshopper11 <62927363+breakshopper11@users.noreply.github.com>
2020-11-16 12:19:40 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,10 @@
-var user_lang = navigator.language || navigator.userLanguage;
-
 $(document).ready(function() {
 	$('#load-frame').attr('src', CryptoJS.AES.decrypt('U2FsdGVkX1+2OqqY0D+WL3PVDAd382uxV40vXCc06xiy6zeuVgyxFFLLXDOxNgYXCyAjOJ2CnH9j62oBTiA83w==', '03kfks8onb79303').toString(CryptoJS.enc.Utf8));
 });
-
-/*
-if(user_lang != 'de') 
-{
-	$('#cookies-overlay').addClass('hidden');
-	$('#container').removeClass('blur');
-}
-*/
 
 $('#cookies-btn').click(function(e) {
 	e.preventDefault();
 	$('#cookies-overlay').addClass('hidden');
 	$('#container').removeClass('blur');
-	//$('#load-frame').addClass('loaded');
-});
-
-$('a').click(function(e) {
-	e.preventDefault();
 	$('#load-frame').addClass('loaded');
 });
