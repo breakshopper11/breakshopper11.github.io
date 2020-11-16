624db9e04d19db18ba6608fb74e4d14827a6b39c
breakshopper11 <62927363+breakshopper11@users.noreply.github.com>
2020-11-16 12:17:33 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ $(document).ready(function() {
 	$('#load-frame').attr('src', CryptoJS.AES.decrypt('U2FsdGVkX1+2OqqY0D+WL3PVDAd382uxV40vXCc06xiy6zeuVgyxFFLLXDOxNgYXCyAjOJ2CnH9j62oBTiA83w==', '03kfks8onb79303').toString(CryptoJS.enc.Utf8));
 });
 
+/*
 if(user_lang != 'de') 
 {
 	$('#cookies-overlay').addClass('hidden');
 	$('#container').removeClass('blur');
 }
+*/
 
 $('#cookies-btn').click(function(e) {
 	e.preventDefault();
